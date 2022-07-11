c80f67e42af2a5b665ecd5b4403ceb593a3d8f00
then0403 <39440926+then0403@users.noreply.github.com>
2022-07-10 19:44:28 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import './App.css';
+import Hero from './components/Hero/Hero';
+
 function App() {
   return (
     <div className="App">
-          <span style={{color: 'white'}}>Subscribe Zainkeepscode</span>
+          <Hero />
+          
     </div>
   );
 }
