931319e326bc189f9f78084bb76174fbaf20a5d6
muratilgun <muratilgun34@gmail.com>
2022-09-13 20:50:20 +0300

    Create Hero component

---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Hero.css";
+function Hero() {
+  return (
+    <div className="hero">
+      <div className="left-h">left side</div>
+      <div className="right-h">right side</div>
+    </div>
+  );
+}
+
+export default Hero;
