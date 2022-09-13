429a17341bb79c3f50cf0b568ec475dcfd0c15b0
muratilgun <muratilgun34@gmail.com>
2022-09-14 00:04:52 +0300

    Header css

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,19 @@
 import React from "react";
 import "./Header.css";
-const Header =() =>{
-  return <div>Header</div>;
-}
+import Logo from "../../assets/logo.png";
+const Header = () => {
+  return (
+    <div className="header">
+      <img src={Logo} alt="" className="logo" />
+      <ul className="header-menu">
+        <li>Home</li>
+        <li>Programs</li>
+        <li>Why us</li>
+        <li>Plans</li>
+        <li>Testimonials</li>
+      </ul>
+    </div>
+  );
+};
 
 export default Header;
