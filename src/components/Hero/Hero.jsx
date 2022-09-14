689844e89663495dd435a918b1e0e40d27ce1fca
muratilgun <muratilgun34@gmail.com>
2022-09-14 20:48:05 +0300

    nth-of-type

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -21,7 +21,14 @@ const Hero = () => {
           <div>
             <span>Ideal body</span>
           </div>
+          <div>
+            <span>
+              In here we will help you to shape and build your ideal body and
+              live up your life to fullest
+            </span>
+          </div>
         </div>
+    
       </div>
       <div className="right-h">right side</div>
     </div>
