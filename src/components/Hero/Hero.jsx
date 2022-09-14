b6700a83b2a96db93723f0a0343326519e2d7240
muratilgun <muratilgun34@gmail.com>
2022-09-14 20:15:14 +0300

    hero.css

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -7,6 +7,21 @@ const Hero = () => {
     <div className="hero">
       <div className="left-h">
         <Header />
+        {/* the best ad */}
+        <div className="the-best-ad">
+          <div></div>
+          <span>the best fitness club in the town</span>
+        </div>
+        {/* Hero Heading */}
+        <div className="hero-text">
+          <div>
+            <span>Shape </span>
+            <span>Your</span>
+          </div>
+          <div>
+            <span>Ideal body</span>
+          </div>
+        </div>
       </div>
       <div className="right-h">right side</div>
     </div>
