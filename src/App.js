26d445ffa42a9a5dc3a103e88484eb0ffd722e6b
muratilgun <muratilgun34@gmail.com>
2022-09-15 21:58:53 +0300

    Created programs component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import "./App.css";
 import Hero from "./components/Hero/Hero";
+import Programs from "./components/Programs/Programs";
 function App() {
   return (
     <div className="App">
       <Hero />
+      <Programs />
     </div>
   );
 }
---
+++ src/components/Programs/Programs.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "./Programs.css";
+import { programsData } from "../../data/programsData";
+const Programs = () => {
+  return (
+    <div className="Programs" id="programs">
+      {/* header */}
+      <div className="programs-header">
+        <span className="stroke-text">Explore our</span>
+        <span>Programs</span>
+        <span className="stroke-text">to shape you</span>
+      </div>
+    </div>
+  );
+};
+
+export default Programs;
