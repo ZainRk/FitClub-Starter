b9faec927a3d406f6e6b8ff81659d9ee86ebeda9
muratilgun <muratilgun34@gmail.com>
2022-09-16 20:23:33 +0300

    Create Reasons component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import "./App.css";
 import Hero from "./components/Hero/Hero";
 import Programs from "./components/Programs/Programs";
+import Reasons from "./components/Reasons/Reasons";
 function App() {
   return (
     <div className="App">
       <Hero />
       <Programs />
+      <Reasons/>
     </div>
   );
 }
