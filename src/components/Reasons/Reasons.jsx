65b29eb93a2a84f2b2faed8748fa9cc04a5960df
muratilgun <muratilgun34@gmail.com>
2022-09-16 20:53:43 +0300

    bug fixed

--- src/components/Reasons/Reasons.jsx
+++ src/components/Reasons/Reasons.jsx
@@ -22,7 +22,7 @@ const Reasons = () => {
         <span>some reasons</span>
         <div>
           <span className="stroke-text">why</span>
-          <span>choose us?</span>
+          <span> choose us?</span>
         </div>
       </div>
     </div>
