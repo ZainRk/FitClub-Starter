91e9d39233cbab498b8ef0c2df4d7b48ba1bc137
muratilgun <muratilgun34@gmail.com>
2022-09-16 21:16:30 +0300

    completed reasons section

--- src/components/Reasons/Reasons.jsx
+++ src/components/Reasons/Reasons.jsx
@@ -4,7 +4,7 @@ import image1 from "../../assets/image1.png";
 import image2 from "../../assets/image2.png";
 import image3 from "../../assets/image3.png";
 import image4 from "../../assets/image4.png";
-import bn from "../../assets/nb.png";
+import nb from "../../assets/nb.png";
 import adidas from "../../assets/adidas.png";
 import nike from "../../assets/nike.png";
 import tick from "../../assets/tick.png";
@@ -23,6 +23,39 @@ const Reasons = () => {
         <div>
           <span className="stroke-text">why</span>
           <span> choose us?</span>
+        </div>
+
+        <div className="details-r">
+          <div>
+            <img src={tick} alt=""></img>
+            <span>OVER 140+ EXPERT COACHES</span>
+          </div>
+          <div>
+            <img src={tick} alt="" />
+            <span>TRAIN SMARTER AND FASTER THAN BEFORE</span>
+          </div>
+          <div>
+            <img src={tick} alt="" />
+            <span>1 FREE PROGRAM FOR NEW MEMBER</span>
+          </div>
+          <div>
+            <img src={tick} alt="" />
+            <span>RELIABLE PARTNERS</span>
+          </div>
+        </div>
+        <span
+          style={{
+            color: "var(--gray)",
+            fontWeight: "normal",
+          }}
+        >
+          OUR PARTNERS
+        </span>
+
+        <div className="partners">
+          <img src={nb} alt="" />
+          <img src={adidas} alt="" />
+          <img src={nike} alt="" />
         </div>
       </div>
     </div>
