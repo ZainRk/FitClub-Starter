6de6599955843e2e01992b59b323778ca57eb2c2
muratilgun <muratilgun34@gmail.com>
2022-09-17 14:28:56 +0300

    Created Plans Component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import "./App.css";
 import Hero from "./components/Hero/Hero";
+import Plans from "./components/Plans/Plans";
 import Programs from "./components/Programs/Programs";
 import Reasons from "./components/Reasons/Reasons";
 function App() {
@@ -7,7 +8,8 @@ function App() {
     <div className="App">
       <Hero />
       <Programs />
-      <Reasons/>
+      <Reasons />
+      <Plans />
     </div>
   );
 }
---
+++ src/components/Plans/Plans.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import "./Plans.css";
+const Plans = () => {
+  return <div>Plans</div>;
+};
+
+export default Plans;
